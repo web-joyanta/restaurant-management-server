ec66cd8377b67809cef98c3c426a684cbaf9cc1e
web-joyanta <web.joyanta@gmail.com>
2025-10-23 02:28:14 +0600

    database mongodb successfully connected or implement API endpoint to add new food item to database with user info and validation

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://restaurant-management:eJB6uWsPrPv792aW@cluster0.z1ypfcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+
+    try {
+        const database = client.db("restaurantDB");
+        const foodsCollection = database.collection("foods");
+
+        app.post('/foods', async (req, res) => {
+            const newFood = req.body;
+            const result = await foodsCollection.insertOne(newFood);
+            res.send(result);
+        });
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
